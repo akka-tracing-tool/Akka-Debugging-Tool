f0a743602566552864e28216a30cc3eecdef790c
mariusz89016 <mariusz89016@gmail.com>
2015-04-12 12:55:14 +0200

    Cleaned up

---
+++ src/main/scala/DistributedStackTraceUtil.scala
@@ -0,0 +1,19 @@
+import akka.actor.Actor
+
+trait DistributedStackTraceMessage {
+  val stackTrace = Thread.currentThread().getStackTrace
+}
+
+trait DistributedStackTrace { self: Actor =>
+  override def aroundReceive(receive: Actor.Receive, msg: Any): Unit = {
+    try {
+      receive.applyOrElse(msg, unhandled)
+    } catch {
+      case exception: Exception => println("INTERCEPTED exception " + exception)
+        val oldStackTrace = exception.getStackTrace
+        val newStackTrace = oldStackTrace ++ msg.asInstanceOf[DistributedStackTraceMessage].stackTrace
+        exception.setStackTrace(newStackTrace)
+        throw exception
+    }
+  }
+}
--- src/main/scala/User.scala
+++ src/main/scala/User.scala
@@ -1,4 +1,4 @@
-import UnreliableWorker.{MyTypeOfMessage, MessageWithException}
+import UnreliableWorker.MyTypeOfMessage
 import akka.actor._
 import com.sun.javaws.exceptions.InvalidArgumentException
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -8,58 +8,13 @@ object User {
   def props(unreliableWorker: ActorRef): Props = Props(classOf[User], unreliableWorker)
 }
 
-trait DistributedStackTrace { self: Actor =>
-//  def receiveLogging: PartialFunction[Any, Any] = {
-//    case message: Any =>
-//      println("receiveuje " + message)
-//      message
-//  }
-//  def receiveLogging(fun: Receive): Receive = {
-//    case message: Any =>
-//      println("receiveuje " + message)
-//      fun()
-//  }
-
-  override def aroundReceive(receive: Actor.Receive, msg: Any): Unit = {
-    try {
-      receive.applyOrElse(msg, unhandled)
-    } catch {
-      case exception: Exception => println("INTERCEPTED exception " + exception)
-        val oldStackTrace = exception.getStackTrace
-        val newStackTrace = oldStackTrace ++ msg.asInstanceOf[DistributedStackTraceMessage].stackTrace
-        exception.setStackTrace(newStackTrace)
-        throw exception
-    }
-  }
-}
 class User(unreliableWorker: ActorRef) extends Actor with DistributedStackTrace {
   context.system.scheduler.schedule(1 seconds, 2 seconds, unreliableWorker, "value")
-//  def receive: Receive = receiveLogging andThen partialTraitDistributedStackTrace()
+
   def receive: Receive = {
     case MyTypeOfMessage(1) => println("wszystko ok!")
-    case message @ MyTypeOfMessage(0) =>
+    case MyTypeOfMessage(0) =>
       throw new InvalidArgumentException(Array("0 instead 1"))
-  }
-  def partialTraitDistributedStackTrace: Receive = {
-    case MyTypeOfMessage(1) => println("wszystko ok!")
-    case message @ MyTypeOfMessage(0) =>
-//      val exception = new InvalidArgumentException(Array("0 instead 1"))
-//      val oldStackTrace = exception.getStackTrace
-//      val newStackTrace = oldStackTrace ++ message.stackTrace
-//      exception.setStackTrace(newStackTrace)
-//      throw exception
-
-    throw new InvalidArgumentException(Array("0 instead 1"))
-  }
-
-  def partialMessageWithException: Receive = {
-    case MessageWithException(1) => println("wszystko ok!")
-    case message @ MessageWithException(0) =>
-      val exception = new InvalidArgumentException(Array("0 instead 1"))
-      val oldStackTrace = exception.getStackTrace
-      val newStackTrace = oldStackTrace ++ message.stackTrace
-      exception.setStackTrace(newStackTrace)
-      throw exception
   }
 }
 
