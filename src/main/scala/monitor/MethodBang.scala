f4aff1de6108b0d74bc5ac1f0f375e947436e37e
mariusz89016 <mariusz89016@gmail.com>
2015-05-10 21:54:47 +0200

    Written working example usages of AspectJ

--- src/main/scala/monitor/MethodBang.scala
+++ src/main/scala/monitor/MethodBang.scala
@@ -1,13 +1,36 @@
 package monitor
 
-import org.aspectj.lang.ProceedingJoinPoint
-import org.aspectj.lang.annotation.{Around, Aspect}
+import org.aspectj.lang.{JoinPoint, ProceedingJoinPoint}
+import org.aspectj.lang.annotation._
 
 @Aspect
 class MethodBang {
-  @Around("execution(* akka.actor.ScalaActorRef.*(..))")
-  def bangMethod(joinPoint: ProceedingJoinPoint): Any = {
-    println("SIEMANDERO")
-    joinPoint.proceed()
+
+  @Before("execution(* akka.actor.ScalaActorRef.$bang(..))")
+  def beforeBangMethod(joinPoint: JoinPoint): Unit = {
+    println("beforeBangMethod: " + joinPoint.getSignature.getName)
+  }
+
+  @After("execution(* akka.actor.ScalaActorRef.$bang(..))")
+  def afterBangMethod(joinPoint: JoinPoint): Unit = {
+    println("afterBangMethod: " + joinPoint.getSignature.getName)
+  }
+
+  @AfterReturning(pointcut = "execution(int scala.util.Random.nextInt())", returning = "result")
+  def afterReturningRandomNextInt(jointPoint: JoinPoint, result: Any) {
+    println("afterReturningRandomNextInt: " + result.toString)
+  }
+
+  @AfterThrowing(pointcut = "execution(* User.aroundReceive(..))", throwing = "error")
+  def afterThrowingMethod(joinPoint: JoinPoint, error: Throwable): Unit = {
+    println("afterThrowingMethod: " + error.toString)
+  }
+
+  @Around("execution(* scala.util.Random.nextInt())")
+  def aroundMethod(joinPoint: ProceedingJoinPoint): Any = {
+    println("before Random.nextInt()")
+    val result = joinPoint.proceed()
+    println("after Random.nextInt()")
+    result
   }
 }
