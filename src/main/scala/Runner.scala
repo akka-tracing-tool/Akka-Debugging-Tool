bfb165d934cf705b5815538e45d9274a05c18fd8
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2015-05-29 10:04:41 +0200

    [PPAKKAD-13] Removed unnecessary code.

--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -1,11 +1,8 @@
 import akka.actor.{ActorSystem, Props}
 import akka_debugging.database.DatabaseUtils
-import com.typesafe.config._
 
 object Runner {
   def main(args: Array[String]) {
-    val configFile = "remote_application.conf"
-    val config = ConfigFactory.load(configFile)
     DatabaseUtils.init()
     val system = ActorSystem()
     val unreliableWorkerRef = system.actorOf(Props[UnreliableWorker], "unreliableWorker")
